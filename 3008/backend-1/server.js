a021ac7af8ab50eea5eadde2b58e0bcd29d59b73
Priyanshu Saini <priyanshu688.be22@chitkara.edu.in>
2024-08-31 22:42:35 +0530

    ok

--- 3008/backend-1/server.js
+++ 3008/backend-1/server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(4000, () => {
-    console.log(`app listening on port 4000`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
